736871b8eabe6798e856c73af142aa9428c79964
Kien-Hoang2692002 <92370459+Kien-Hoang2692002@users.noreply.github.com>
2023-11-17 09:22:49 +0700

    mode-config

---
+++ src/theme.js
@@ -0,0 +1,41 @@
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+import { teal, deepOrange, cyan, orange } from "@mui/material/colors";
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange,
+      },
+    },
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange,
+      },
+    },
+  },
+  // ...other properties
+});
+
+export default theme;
+
+// // Create a theme instance.
+// const theme = createTheme({
+//   palette: {
+//     mode: "light",
+//     primary: {
+//       main: "#556cd6",
+//     },
+//     secondary: {
+//       main: "#19857b",
+//     },
+//     error: {
+//       main: red.A400,
+//     },
+//     text: {
+//       secondary: red[500],
+//     },
+//   },
+// });
